cd3fc3d875e6d84323c55c0bb19f2dd23d43c81e
JulienLoe <julien.loeuilleux62@gmail.com>
2023-01-29 18:30:44 +0100

    new Copy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = 8080;
 const app = express();
-require('dotenv').config();
+// require('dotenv').config();
 const configDB = require('./configDB/configDB.js')
 const Product = require('./models/products')
 const cors = require('cors');
